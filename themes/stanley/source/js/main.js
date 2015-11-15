c9798585ce80f27ddb7ce12a6425e93bc541bbaf
Andre Martins <andrepm@gmail.com>
2015-11-15 20:14:36 +0100

    convert main.js and main.css inline

--- themes/stanley/source/js/main.js
+++
@@ -1,8 +0,0 @@
-// Highlight current section in menu
-$(document).ready(function() {
-	var currentNav = $('.nav a[href="' + this.location.pathname + '"]').parent();
-	currentNav.addClass('active');
-    if (currentNav.parent().parent().attr('class').indexOf('dropdown') >= 0) {
-    	currentNav.parent().parent().addClass('active');
-    }
-});
